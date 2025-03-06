75336307d24ba35f0fa35dd1f48baf48bc0558dd
AxelSevenS <spaghettiandmeatballs61@gmail.com>
2025-03-06 14:26:44 +0100

    refactor: Replace VerticalStackLayout with ListView for message display

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using MQTTnet;
 
@@ -6,7 +8,11 @@ namespace MqttDashboard;
 
 public partial class MainPage : ContentPage
 {
+	private static readonly string STEAM_TOPIC = "test/topic";
+
+
 	private readonly IMqttClient mqttClient;
+	private readonly ObservableCollection<string> steamHistory = [];
 
 	public MainPage()
 	{
@@ -14,6 +20,7 @@ public partial class MainPage : ContentPage
 		mqttClient = factory.CreateMqttClient();
 
 		InitializeComponent();
+		SteamMessages.ItemsSource = steamHistory;
 		ConnectToMqttBroker();
 	}
 
@@ -29,20 +36,28 @@ public partial class MainPage : ContentPage
 
 			mqttClient.ApplicationMessageReceivedAsync += async e =>
 			{
-				string message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
 				string topic = e.ApplicationMessage.Topic;
+				string message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
 
 				// Update UI with new message
 				MainThread.BeginInvokeOnMainThread(() =>
 				{
-					MessagesLabel.Text += $"\n[{topic}]: {message}";
+					if (topic == STEAM_TOPIC) {
+						steamHistory.Insert(0, message);
+					}
 				});
 			};
 
 			mqttClient.ConnectedAsync += async e =>
 			{
 				Console.WriteLine("Connected to MQTT broker!");
-				await mqttClient.SubscribeAsync("test/topic"); // Subscribe to a test topic
+				await mqttClient.SubscribeAsync(STEAM_TOPIC);
+			};
+
+			mqttClient.DisconnectedAsync += async e =>
+			{
+				Console.WriteLine("Disconnected from MQTT broker!");
+				await mqttClient.ConnectAsync(options);
 			};
 
 			await mqttClient.ConnectAsync(options);
